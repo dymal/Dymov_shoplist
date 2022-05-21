a3ccc592925d350dd63d302f3a9fd465117c6ad9
dymal <105967067+dymal@users.noreply.github.com>
2022-05-21 10:56:30 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,322 @@
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var third = document.getElementById('third');
+
+
+var close = document.getElementById('myClose');
+var close2 = document.getElementById('myClose2');
+var close3 = document.getElementById('myClose3');
+var close4 = document.getElementById('myClose4');
+var close5 = document.getElementById('myClose5');
+var close6 = document.getElementById('myClose6'); 
+var close7 = document.getElementById('myClose7');
+var close8 = document.getElementById('myClose8');
+var close9 = document.getElementById('myClose9');
+var close10 = document.getElementById('myClose10');
+
+var block = document.getElementById('block');
+var block2 = document.getElementById('block2');
+var block3 = document.getElementById('block3');
+var block4 = document.getElementById('block4');
+var block5 = document.getElementById('block5');
+var block6 = document.getElementById('block6');
+var block7 = document.getElementById('block7');
+var block8 = document.getElementById('block8');
+var block9 = document.getElementById('block9');
+var block10 = document.getElementById('block10');
+
+var plus = document.getElementById('myPlus');
+var plus2 = document.getElementById('myPlus2');
+var plus3 = document.getElementById('myPlus3');
+var plus4 = document.getElementById('myPlus4');
+var plus5 = document.getElementById('myPlus5');
+var plus6 = document.getElementById('myPlus6');
+var plus7 = document.getElementById('myPlus7');
+var plus8 = document.getElementById('myPlus8');
+var plus9 = document.getElementById('myPlus9');
+var plus10 = document.getElementById('myPlus10');
+
+
+var gal = document.getElementById('myGal');
+var gal2 = document.getElementById('myGal2');
+var gal3 = document.getElementById('myGal3');
+var gal4 = document.getElementById('myGal4');
+var gal5 = document.getElementById('myGal5');
+var gal6 = document.getElementById('myGal6');
+var gal7 = document.getElementById('myGal7');
+var gal8 = document.getElementById('myGal8');
+var gal9= document.getElementById('myGal9');
+var gal10 = document.getElementById('myGal10');
+
+var sector = document.getElementById('sector');
+var sector2 = document.getElementById('sector2');
+var sector3 = document.getElementById('sector3');
+var sector4 = document.getElementById('sector4');
+var sector5 = document.getElementById('sector5');
+var sector6 = document.getElementById('sector6');
+var sector7 = document.getElementById('sector7');
+var sector8 = document.getElementById('sector8');
+var sector9 = document.getElementById('sector9');
+var sector10 = document.getElementById('sector10');
+
+
+var photo = document.getElementById('myPhoto');
+var photo2 = document.getElementById('myPhoto2');
+var photo3 = document.getElementById('myPhoto3');
+var photo4 = document.getElementById('myPhoto4');
+var photo5 = document.getElementById('myPhoto5');
+var photo6 = document.getElementById('myPhoto6');
+var photo7 = document.getElementById('myPhoto7');
+var photo8 = document.getElementById('myPhoto8');
+var photo9 = document.getElementById('myPhoto9');
+var photo10 = document.getElementById('myPhoto10');
+
+var modal = document.getElementById('myModal');
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+var modal4 = document.getElementById('myModal4');
+var modal5 = document.getElementById('myModal5');
+var modal6 = document.getElementById('myModal6');
+var modal7 = document.getElementById('myModal7');
+var modal8 = document.getElementById('myModal8');
+var modal9 = document.getElementById('myModal9');
+var modal10 = document.getElementById('myModal10');
+
+var kr=document.getElementById('myKr');
+var kr2=document.getElementById('myKr2');
+var kr3=document.getElementById('myKr3');
+var kr4=document.getElementById('myKr4');
+var kr5=document.getElementById('myKr5');
+var kr6=document.getElementById('myKr6');
+var kr7=document.getElementById('myKr7');
+var kr8=document.getElementById('myKr8');
+var kr9=document.getElementById('myKr9');
+var kr10=document.getElementById('myKr10');
+
+gal.onclick = function() {
+    gal.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal2.onclick = function() {
+    gal2.style.backgroundColor='green';
+ 
+    setTimeout(dis, 10);  
+}
+gal3.onclick = function() {
+    gal3.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal4.onclick = function() {
+    gal4.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal5.onclick = function() {
+    gal5.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal6.onclick = function() {
+    gal6.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal7.onclick = function() {
+    gal7.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal8.onclick = function() {
+    gal8.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal9.onclick = function() {
+    gal9.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+gal10.onclick = function() {
+    gal10.style.backgroundColor='green';
+    setTimeout(dis, 10);  
+}
+
+
+
+
+
+plus.onclick = function(){
+    sector2.style.display='block';
+    plus.style.backgroundColor='white';
+
+}
+plus2.onclick = function(){
+    sector3.style.display='block';
+    plus2.style.backgroundColor='white';
+
+}
+plus3.onclick = function(){
+    sector4.style.display='block';
+    plus3.style.backgroundColor='white';
+
+}
+plus4.onclick = function(){
+    sector5.style.display='block';
+    plus4.style.backgroundColor='white';
+
+}
+plus5.onclick = function(){
+    sector6.style.display='block';
+    plus5.style.backgroundColor='white';
+
+}
+plus6.onclick = function(){
+    sector7.style.display='block';
+    plus6.style.backgroundColor='white';
+
+}
+plus7.onclick = function(){
+    sector8.style.display='block';
+    plus7.style.backgroundColor='white';
+
+}
+plus8.onclick = function(){
+    sector9.style.display='block';
+    plus8.style.backgroundColor='white';
+
+}
+plus9.onclick = function(){
+    sector10.style.display='block';
+    plus9.style.backgroundColor='white';
+
+}   
+
+
+
+
+
+
+
+
+close.onclick = function(){
+    sector.style.display="none";
+}
+close2.onclick = function(){
+    sector2.style.display="none";
+    plus.style.backgroundColor='blue';
+}
+close3.onclick = function(){
+    sector3.style.display="none";
+    plus2.style.backgroundColor='blue';
+}
+close4.onclick = function(){
+    sector4.style.display="none";
+    plus3.style.backgroundColor='blue';
+}
+close5.onclick = function(){
+    sector5.style.display="none";
+    plus4.style.backgroundColor='blue';
+}
+close6.onclick = function(){
+    sector6.style.display="none";
+    plus5.style.backgroundColor='blue';
+}
+close7.onclick = function(){
+    sector7.style.display="none";
+    plus6.style.backgroundColor='blue';
+}
+close8.onclick = function(){
+    sector8.style.display="none";
+    plus7.style.backgroundColor='blue';
+}
+close9.onclick = function(){
+    sector9.style.display="none";
+    plus8.style.backgroundColor='blue';
+}
+close10.onclick = function(){
+    sector10.style.display="none";
+    plus9.style.backgroundColor='blue';
+}
+
+
+
+function dis(){
+    first.disabled = true;
+    second.disabled = true;
+    third.disabled = true;
+}
+
+function undis(){
+    first.disabled = false;
+    second.disabled = false;
+    third.disabled = false;
+}
+
+
+/*1*/ 
+photo.onclick=function(){
+    modal.style.display='block';
+}
+kr.onclick=function(){
+    modal.style.display='none';
+}
+
+/*2*/ 
+photo2.onclick=function(){
+    modal2.style.display='block';
+}
+kr2.onclick=function(){
+    modal2.style.display='none';
+}
+
+/*3*/ 
+photo3.onclick=function(){
+    modal3.style.display='block';
+}
+kr3.onclick=function(){
+    modal3.style.display='none';
+}
+
+/*4*/ 
+photo4.onclick=function(){
+    modal4.style.display='block';
+}
+kr4.onclick=function(){
+    modal4.style.display='none';
+}
+/*5*/ 
+photo5.onclick=function(){
+    modal5.style.display='block';
+}
+kr5.onclick=function(){
+    modal5.style.display='none';
+}
+/*6*/ 
+photo6.onclick=function(){
+    modal6.style.display='block';
+}
+kr6.onclick=function(){
+    modal6.style.display='none';
+}
+/*7*/ 
+photo7.onclick=function(){
+    modal7.style.display='block';
+}
+kr7.onclick=function(){
+    modal7.style.display='none';
+}
+/*8*/ 
+photo8.onclick=function(){
+    modal8.style.display='block';
+}
+kr8.onclick=function(){
+    modal8.style.display='none';
+}
+/*9*/ 
+photo9.onclick=function(){
+    modal9.style.display='block';
+}
+kr9.onclick=function(){
+    modal9.style.display='none';
+}
+/*10*/ 
+photo10.onclick=function(){
+    modal10.style.display='block';
+}
+kr10.onclick=function(){
+    modal10.style.display='none';
+}
